2adea60406e0bf3dcc5a58385576d89009fa445a
Isom92 <ikamilov0927@sdsu.edu>
2022-03-08 15:13:40 -0800

    Update LinearRail.h

--- src/LinearRail.h
+++ src/LinearRail.h
@@ -11,18 +11,24 @@
 class LinearRail{
     public:
         // constructor
-        LinearRail(uint8_t flap_pin, uint8_t rail_pin);
+        LinearRail(uint8_t flap_pin, uint8_t rail_dir_pin, uint8_t rail_step_pin);
 
         // functions
         void dispense_by_day(uint8_t day);
+	// the fuctions to grab pins ???
+
+
     private:
         // helper functions
         void _move_stepper(int movedSteps, bool direction);
         void _drop_pill();
 
         // pinouts
-        uint8_t _flap_pin;
-        uint8_t _rail_pin;
+        uint8_t _flap_pin;   //might not need this if using pin grabbing function
+        uint8_t _rail_step_pin;
+	uint8_t _rail_dir_pin;
+	
+
 
         // physical components
         LinearRailFlap _lr_flap;
